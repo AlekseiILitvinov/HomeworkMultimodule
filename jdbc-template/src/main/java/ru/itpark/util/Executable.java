10f83d6e7b6e3575b9291a94908de2a4fca1714c
AlekseiILitvinov <litvinovai988@gmail.com>
2019-11-28 20:13:31 +0300

    Removed jdbc template module from project

--- jdbc-template/src/main/java/ru/itpark/util/Executable.java
+++
@@ -1,9 +0,0 @@
-package ru.itpark.util;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-@FunctionalInterface
-public interface Executable<T> {
-    T execute(ResultSet resultSet) throws SQLException;
-}
--- jdbc-template/src/main/java/ru/itpark/util/PreparedStatementSetter.java
+++
@@ -1,9 +0,0 @@
-package ru.itpark.util;
-
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
-@FunctionalInterface
-public interface PreparedStatementSetter {
-    PreparedStatement set(PreparedStatement statement) throws SQLException;
-}
